d6f83ec55093238776d4872e80b0928d666f5990
Muhammad Fakhri Abdillah <59867082+mfakhriabdillah@users.noreply.github.com>
2023-05-17 22:45:02 +0700

    Add files via upload

---
+++ deploy-go/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Golang Sample, Selamat Datang!\nApp Version: 6\nBelajar Jenkins Deploy Golang using stash")
+}
+
+func envPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List of environment variable:")
+	for _, env := range os.Environ() {
+		fmt.Fprintf(w, "- "+env+"\n")
+	}
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/env", envPage)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func main() {
+	handleRequests()
+}
